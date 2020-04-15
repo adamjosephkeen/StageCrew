a47bf36a1709bc1fcb496ff94988343b0d0f6a61
adamjosephkeen <44175024+adamjosephkeen@users.noreply.github.com>
2020-04-15 15:56:21 -0400

    Add files via upload
    StageCrew IOS

---
+++ StageCrew.iOS/Data/SQLite_IOS.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using SQLite;
+
+using Foundation;
+using StageCrew.Data;
+using UIKit;
+using Xamarin.Forms;
+using StageCrew.iOS.Data;
+
+[assembly: Dependency(typeof(SQLite_IOS))]
+
+namespace StageCrew.iOS.Data
+{
+    public class SQLite_IOS : ISQLite
+    {
+        public SQLite_IOS() { }
+        public SQLite.SQLiteConnection GetConnection()
+        {
+            var fileName = "Testdb.db3";
+            var documentPath = Environment.GetFolderPath(Environment.SpecialFolder.Personal);
+            var libraryPath = Path.Combine(documentPath, "..", "Library");
+            var path = Path.Combine(libraryPath, fileName);
+            var connection = new SQLite.SQLiteConnection(path);
+            return connection;
+        }
+    }
+}
